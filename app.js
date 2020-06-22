7c65a73c5603389055f34d05d38e88066b424ea0
Sabrina Snider <sabrina.snider10@gmail.com>
2020-06-22 17:58:49 -0400

    Removed old code from the nodejs template.

--- app.js
+++ app.js
@@ -17,22 +17,7 @@ app.set('view engine', 'handlebars');
 app.use(express.urlencoded());
 app.use('/static', express.static('public'));
 
-// View configs.
-var index = require('./routes/index');
-var client_demo = require('./routes/client_demo');
-
-app.get('/', index.view, function (req, res) {
-  res.send(req.params)
-});
-
-// Route path configs.
-app.get('/fname/:firstName/lname/:lastName', index.view, function (req, res) {
-  res.send(req.params)
-});
-
-app.get('/client_demo', client_demo.view, function (req, res) {
-  res.send(req.params)
-});
+app.get('/', (req, res) => res.render('index'));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
