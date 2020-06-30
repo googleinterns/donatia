e1921d63ee8930c7fd157fe22fc9856f3d628fa8
Sabrina Snider <sabrina.snider10@gmail.com>
2020-06-30 14:06:25 -0400

    Only open one info window at a time.

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,8 +1,11 @@
+const HOUSTON_CORDS = { lat: 29.7604, lng: -95.3698 }
 var map;
+var bounds;
+var openMarkerWindow;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 29.7604, lng: -95.3698 },
+    center: HOUSTON_CORDS,
     zoom: 13,
     disableDefaultUI: true,
     zoomControl: true,
@@ -13,12 +16,11 @@ function initMap() {
       }
     ]
   });
+  bounds = new google.maps.LatLngBounds(HOUSTON_CORDS);
   generateMarkers();
 }
 
 function generateMarkers() {
-  console.log("making markers")
-
   organizations.forEach(org => {
     var marker = new google.maps.Marker({
       position: new google.maps.LatLng(org.latitude, org.longitude),
@@ -26,12 +28,17 @@ function generateMarkers() {
       title: org.title
     });
 
-    var infoWindow = new google.maps.InfoWindow({
+    var markerWindow = new google.maps.InfoWindow({
       content: org.title
     });
 
     google.maps.event.addListener(marker, 'click', function () {
-      infoWindow.open(map, marker);
+      if (openMarkerWindow) openMarkerWindow.close();
+      openMarkerWindow = markerWindow;
+      openMarkerWindow.open(map, marker);
     });
+
+    bounds.extend(marker.getPosition());
+    map.fitBounds(bounds);
   });
 }
