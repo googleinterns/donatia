2b103ac9cc589c183e81b52c37b7aaafd451771f
Daniel Abreo <dabreo43@gmail.com>
2020-07-01 10:34:11 -0500

    Combine view/edit pages

--- public/js/dashboardInfoForm.js
+++ public/js/dashboardInfoForm.js
@@ -4,27 +4,28 @@
  * @return JSON of organization's information 
  */
 function getFormInfo(id) {
-    var info = {
-        "ID": "google1",
-        "name": "Google",
-        "placesID": "12345 Road Drive",
-        "description": "a great place to find non profits!",
-        "phone": "1234567890",
-        "email": "org@google.com",
-        "websiteURL": "google.com",
-    }
-    return info;
+  const info = {
+    "ID": "google1",
+    "name": "Google",
+    "placesID": "12345 Road Drive",
+    "description": "a great place to find non profits!",
+    "phone": "1234567890",
+    "email": "org@google.com",
+    "websiteURL": "google.com",
+  }
+  return info;
 }
 
 /**
- * Pre-populates the input elements of the form.
+ * Populates the input elements of the form.
+ * Removes disabled attribute if on edit page
  */
 window.onload = async function populateForm() {
-    var formJSON = await getFormInfo("{{id}}");
-    document.getElementById("organization-name").value = formJSON.name;
-    document.getElementById("organization-description").value = formJSON.description;
-    document.getElementById("organization-address").value = formJSON.placesID;
-    document.getElementById("organization-phone").value = formJSON.phone;
-    document.getElementById("organization-website").value = formJSON.websiteURL;
-    document.getElementById("organization-email").value = formJSON.email;
+  const formJSON = await getFormInfo("{{id}}");
+  document.getElementById("organization-name").value = formJSON.name;
+  document.getElementById("organization-description").value = formJSON.description;
+  document.getElementById("organization-address").value = formJSON.placesID;
+  document.getElementById("organization-phone").value = formJSON.phone;
+  document.getElementById("organization-website").value = formJSON.websiteURL;
+  document.getElementById("organization-email").value = formJSON.email;
 }
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,16 +2,16 @@
  * GET client-side rendering dashboard page.
  */
 
-exports.view = function(req, res) {
-  var pageParam = req.params.page;
-  var pageName = 'dashboardView';
-  if (pageParam === 'edit') pageName = 'dashboardEdit';
+exports.view = function (req, res) {
+  const pageParam = req.params.page;
+  var pageName = 'dashboardProfile';
 
   res.render(
     pageName,
     {
       layout: 'dashboard',
       id: req.params.id,
+      editable: pageParam === 'edit',
     }
   )
 }
