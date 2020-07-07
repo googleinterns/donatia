978653cf9a491fd77a8a2c33990b9ae2a431ec63
Malcolm Jones <malcolmjones@google.com>
2020-07-07 09:12:36 -0400

    Fix formatting

--- routes/data.js
+++ routes/data.js
@@ -4,52 +4,53 @@ const {Firestore} = require('@google-cloud/firestore');
 const firestore = new Firestore();
 
 exports.view = function(req, res) {
-    createMovies();
-    const movies = readMovies();
-    movies.then((data) => {
-        res.send(data);
-    })
+  createMovies();
+  const movies = readMovies();
+  movies.then((data) => {
+    res.send(data);
+  })
 };
 
+// Inserts 3 new documents into the 'movies' collection with hard-coded data 
 async function createMovies() {
-    // Dummy data to insert into the database
-    const movieData = [
-        {
-            title: "Black Panther",
-            year: 2018,
-        },
-        {
-            title: "Avengers: Endgame",
-            year: 2019,
-        },
-        {
-            title: "Iron Man",
-            year: 2008,
-        }
-    ];
-
-    movieData.forEach((movie) => {
-        let document = firestore.collection('movies').doc();
-        document.set(movie);
-    });
+  // Dummy data to insert into the database
+  const movieData = [
+    {
+      title: "Black Panther",
+      year: 2018,
+    },
+    {
+      title: "Avengers: Endgame",
+      year: 2019,
+    },
+    {
+      title: "Iron Man",
+      year: 2008,
+    }
+  ];
+  
+  movieData.forEach((movie) => {
+    let document = firestore.collection('movies').doc();
+    document.set(movie);
+  });
 }
 
 // Read all documents in the 'movies' collection
 async function readMovies() {
-    let movieList = [];
-    const moviesRef = firestore.collection('movies');
-    const snapshot = await moviesRef.get();
-    snapshot.forEach((movie) => {
-        movieList.push(movie.data());
-    });
-    return movieList;
+  let movieList = [];
+  const moviesRef = firestore.collection('movies');
+  const snapshot = await moviesRef.get();
+  snapshot.forEach((movie) => {
+    movieList.push(movie.data());
+  });
+  return movieList;
 }
 
+// Deletes all documents in the 'movies' collection
 async function deleteAllMovies() {
-    let movieList = [];
-    const moviesRef = firestore.collection('movies');
-    const snapshot = await moviesRef.get();
-    snapshot.forEach((movie) => {
-        movie.ref.delete();
-    });
+  const moviesRef = firestore.collection('movies');
+  const snapshot = await moviesRef.get();
+  snapshot.forEach((movie) => {
+    movie.ref.delete();
+  });
 }
