ce5d0d1d8f065698263619a0b06d845e702beb7c
Daniel Abreo <dabreo43@gmail.com>
2020-07-13 10:56:38 -0500

    Add category instruction feature

--- public/js/dashboardCategories.js
+++ public/js/dashboardCategories.js
@@ -68,6 +68,69 @@ window.deleteCategory = function deleteCategory(categoryID) {
   console.log('deleted' + categoryID);
 };
 
+/**
+ * Adds an input box to the correct section of the page.
+ * @param {string} prevInstructionID ID of the previous input
+ * box's add button [instructionType]-add-[instructionIndex].
+ */
+function addInstructionInput(prevInstructionID) {
+  const prevInstructionIdArr = prevInstructionID.split('-');
+  const instructionType = prevInstructionIdArr[0];
+  const currInstructionIndex = parseInt(prevInstructionIdArr[2]) + 1;
+
+  const prevRemoveButton = document.getElementById(
+    instructionType + '-remove-' + prevInstructionIdArr[2]
+  );
+  if (prevRemoveButton) prevRemoveButton.hidden = false;
+
+  const prevAddButton = document.getElementById(
+    instructionType + '-add-' + prevInstructionIdArr[2]
+  );
+  if (prevAddButton) prevAddButton.hidden = true;
+
+  const currInstructionDiv = document.createElement('div');
+  currInstructionDiv.setAttribute('id', instructionType + '-container-' + currInstructionIndex);
+
+  const inputBox = document.createElement('input');
+  inputBox.setAttribute('id', instructionType + '-input-' + currInstructionIndex);
+  inputBox.setAttribute('type', 'text');
+  inputBox.setAttribute('name', instructionType + '-input-' + currInstructionIndex);
+
+  const removeButton = document.createElement('button');
+  removeButton.innerHTML = '-';
+  removeButton.setAttribute('id', instructionType + '-remove-' + currInstructionIndex);
+  removeButton.setAttribute('onclick', 'removeInstructionInput(this.id)');
+  removeButton.hidden = true;
+
+  const addButton = document.createElement('button');
+  addButton.innerHTML = '+';
+  addButton.setAttribute('id', instructionType + '-add-' + currInstructionIndex);
+  addButton.setAttribute('onclick', 'addInstructionInput(this.id)');
+
+  const breakElement = document.createElement('br');
+
+  currInstructionDiv.append(inputBox);
+  currInstructionDiv.append(removeButton);
+  currInstructionDiv.append(addButton);
+  currInstructionDiv.append(breakElement);
+
+  const instructionContainer = document.getElementById(instructionType + '-container');
+  instructionContainer.append(currInstructionDiv);
+}
+
+/**
+ * Removes the input container of the given button.
+ * @param {string} instructionID ID of the current input box's
+ * remove button [instructionType]-add-[instructionIndex].
+ */
+window.removeInstructionInput = function removeInstructionInput(instructionID) {
+  const instructionIdArr = instructionID.split('-');
+  const instructionContainer = document.getElementById(
+    instructionIdArr[0] + '-container-' + instructionIdArr[2]
+  );
+  instructionContainer.remove();
+};
+
 window.onload = async function () {
   const addCategoriesTemplate = document.getElementById('add-categories-template').innerHTML;
   const renderAddCategories = Handlebars.compile(addCategoriesTemplate);
@@ -82,4 +145,7 @@ window.onload = async function () {
   this.document.getElementById('existing-card-holder').innerHTML = renderExistingCards({
     cardList: allExistingCards,
   });
+
+  addInstructionInput('quality-add-0');
+  addInstructionInput('donation-add-0');
 };
