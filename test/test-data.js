64541b3828cc8bc802d02c809d506baab65f6aad
Malcolm Jones <malcolmjones@google.com>
2020-07-13 12:15:42 -0400

    Remove unused

--- test/test-data.js
+++ test/test-data.js
@@ -1,5 +1,4 @@
 const expect = require('chai').expect;
-const assert = require('assert');
 const request = require('request');
 const data = require('../routes/data');
 const FirestoreMock = require('firestore-mock');
