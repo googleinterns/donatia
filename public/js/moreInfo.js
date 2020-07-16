9d7502f951fa115f3d795368de759ad02da4b4b7
Malcolm Jones <malcolmjones@google.com>
2020-07-16 13:10:29 -0400

    Add organization info template

---
+++ public/js/moreInfo.js
@@ -0,0 +1,17 @@
+const organizationInfoTemplate =
+`
+<h1>{{organization.name}}</h1>
+<div id="contact-section"> 
+  <p><ion-icon name="map-outline"></ion-icon> 12345 Road Drive, Houston, TX 77007</p>
+  <p><ion-icon name="call-outline"></ion-icon>{{organization.phone}}</p>
+  <p><ion-icon name="globe-outline"></ion-icon>{{organization.website}}</p>
+  <p><ion-icon name="mail-outline"></ion-icon>{{organization.email}}</p>
+</div>
+<div id="description">{{organization.description}}</div>
+`;
+
+const acceptedCategoryCardTemplate = 
+`
+
+`;
+
