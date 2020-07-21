c2906dd77cfded4288bddd2fcfe558390343d194
Malcolm Jones <malcolmjones@google.com>
2020-07-21 13:29:14 -0400

    Add links to organization info that allows user to email, call, go to website

--- public/js/moreInfo.js
+++ public/js/moreInfo.js
@@ -3,10 +3,10 @@
 const organizationInfoTemplate = `
 <h1>{{organization.name}}</h1>
 <div id="contact-section"> 
-  <p><ion-icon name="map-outline"></ion-icon> 12345 Road Drive, Houston, TX 77007</p>
-  <p><ion-icon name="call-outline"></ion-icon>{{organization.phone}}</p>
-  <p><ion-icon name="globe-outline"></ion-icon>{{organization.website}}</p>
-  <p><ion-icon name="mail-outline"></ion-icon>{{organization.email}}</p>
+  <a href=""><ion-icon name="map-outline"></ion-icon> 12345 Road Drive, Houston, TX 77007</a>
+  <a href="tel:{{organization.phone}}"><ion-icon name="call-outline"></ion-icon>{{organization.phone}}</a>
+  <a href="{{organization.website}}"><ion-icon name="globe-outline"></ion-icon>{{organization.website}}</a>
+  <a href="mailto:{{organization.email}}"><ion-icon name="mail-outline"></ion-icon>{{organization.email}}</a>
 </div>
 <div id="description">{{organization.description}}</div>
 `;
