3b074761156e77115c86322c2215aa4a3bff923e
Sabrina Snider <sabrina.snider10@gmail.com>
2020-07-21 15:37:30 -0400

    Ran linter.

--- public/js/discover/autocomplete.js
+++ public/js/discover/autocomplete.js
@@ -15,7 +15,7 @@ const autocompleteOptionTemplate = `
  * @param {string} input The text from the search box.
  */
 window.showAutocompleteOptions = function (input) {
-  console.log("inputted", input)
+  console.log('inputted', input);
   if (input === '') {
     document.getElementById('autocomplete-list').innerHTML = '';
     return;
