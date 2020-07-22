852ed16351dbe9268fddf8d8e9157fbed7e31180
Daniel Abreo <dabreo43@gmail.com>
2020-07-22 13:49:08 -0500

    Convert address to placeID

--- public/js/dashboardInfoForm.js
+++ public/js/dashboardInfoForm.js
@@ -1,3 +1,5 @@
+/* global google */
+
 /**
  * Gets the information of the organization assigned to the current user.
  * @return {JSON} An organization's information.
@@ -9,14 +11,36 @@ async function getOrganizationInfo() {
   return {...orgInfo, orgID: organizationData.id};
 }
 
+/**
+ * Adds Google Maps Places autocomplete to address input.
+ */
+function initAutocomplete() {
+  const addressInput = document.getElementById('address');
+  const autocomplete = new google.maps.places.Autocomplete(addressInput);
+  autocomplete.setFields(['place_id', 'geometry', 'name']);
+  autocomplete.addListener('place_changed', function () {
+    const place = autocomplete.getPlace();
+    if (!place.place_id) return;
+    document.getElementById('placeID').value = place.place_id;
+  });
+}
+
 /*
  * Pre-populates the input elements of the form.
  */
 window.onload = async function populateForm() {
   const formJSON = await getOrganizationInfo();
+
+  // Convert placesID to formatted address.
+  const geocoder = new google.maps.Geocoder();
+  geocoder.geocode({placeId: formJSON.placeID}, (results, status) => {
+    if (status === 'OK' && results[0]) {
+      document.getElementById('address').value = results[0].formatted_address;
+    }
+  });
+
   document.getElementById('name').value = formJSON.name;
   document.getElementById('description').value = formJSON.description;
-  document.getElementById('address').value = formJSON.address;
   document.getElementById('phone').value = formJSON.phone;
   document.getElementById('website').value = formJSON.website;
   document.getElementById('email').value = formJSON.email;
@@ -24,4 +48,5 @@ window.onload = async function populateForm() {
   document.getElementById('acceptsPickUp').checked = formJSON.acceptsPickUp;
   document.getElementById('acceptsShipping').checked = formJSON.acceptsShipping;
   document.getElementById('org-info-form').action = `/data/organizations/${formJSON.orgID}`;
+  initAutocomplete();
 };
