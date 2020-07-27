b972c8dd368b8b7dcfbe8ad8e1406549016d1fce
Malcolm Jones <malcolmjones@google.com>
2020-07-27 13:33:57 -0400

    Make formatPhoneNumber into global function

--- public/js/discover/searchList.js
+++ public/js/discover/searchList.js
@@ -83,16 +83,3 @@ export function selectCard(id = null, scroll = false) {
     }
   }
 }
-
-/**
- * Formats a raw phone number into a +# (###) ### - #### format.
- * @param {number} number The phone number unformatted.
- * @return {string} The formatted phone number.
- */
-function formatPhoneNumber(number) {
-  const regexMatch = number.toString().match(/^(\d{3})(\d{3})(\d{4})$/);
-  if (regexMatch) {
-    return `(${regexMatch[1]}) ${regexMatch[2]}-${regexMatch[3]}`;
-  }
-  return number;
-}
